76ead8622e3c9e92ffaaf77e99dbc87a4e648d7f
Alexis Amadei <alexisamadei@gmail.com>
2024-12-02 15:03:28 +0100

    update script

--- initProject.ps1
+++ initProject.ps1
@@ -1,4 +1,11 @@
+# Build the Docker image
 docker build -t node4a -f Dockerfile .
-docker run -d -p 80:80 --name node4a -v $(pwd):/usr/src/app -it --entrypoint=/bin/bash node4a
+
+# Run the Docker container
+docker run -d -p 80:80 --name node4a -v ${PWD}:/usr/src/app -it --entrypoint=/bin/bash node4a
+
+# Start the Docker container
 docker start node4a
+
+# Execute a bash shell in the running container
 docker exec -it node4a /bin/bash
